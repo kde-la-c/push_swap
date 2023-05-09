cdb8bb98591ddfc1e4b19e64cf6b05be90926206
kde-la-c <kdelacolinaplant@gmail.com>
2023-05-09 11:43:16 +0200

    started error handling

--- push_swap.c
+++ push_swap.c
@@ -3,11 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kde-la-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kde-la-c <kde-la-c@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/05 01:35:28 by kde-la-c          #+#    #+#             */
-/*   Updated: 2023/05/05 01:35:31 by kde-la-c         ###   ########.fr       */
+/*   Created: 2023/05/07 14:36:05 by kde-la-c          #+#    #+#             */
+/*   Updated: 2023/05/07 14:36:08 by kde-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+int	error(int argc, void **argv)
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+		return (-1);
+	}
+	else if (argc == 2)
+	{
+
+	}
+}
+
+int	get_args(int argc, void **argv)
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+		return (-1);
+	}
+
+}
+
+int	main(int argc, void **argv)
+{
+	t_list	*args;
+
+	if (error(argc, argv) == -1)
+		return (0);
+	return (0);
+}
--- push_swap.h
+++ push_swap.h
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kde-la-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kde-la-c <kde-la-c@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/05 01:36:00 by kde-la-c          #+#    #+#             */
-/*   Updated: 2023/05/05 01:36:02 by kde-la-c         ###   ########.fr       */
+/*   Created: 2023/05/07 14:36:28 by kde-la-c          #+#    #+#             */
+/*   Updated: 2023/05/07 14:36:32 by kde-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdarg.h>
+
+typedef struct s_list
+{
+	int				nb;
+	struct s_list	*next;
+}					t_list;
+
+#endif
