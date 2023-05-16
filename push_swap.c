bbb95ebf1fc8032ffa93dcb79a9e91d0d918ea4c
kde-la-c <kdelacolinaplant@gmail.com>
2023-05-16 21:32:39 +0200

    program handles negative numbers

--- push_swap.c
+++ push_swap.c
@@ -12,6 +12,12 @@
 
 #include "push_swap.h"
 
+void	dlfree(t_list **lst, char **split)
+{
+	ft_dfree((void **)split);
+	ft_lstclear(&(*lst), free);
+}
+
 t_list	*get_arg(char *arg, t_list *ret)
 {
 	int		i;
@@ -27,34 +33,32 @@ t_list	*get_arg(char *arg, t_list *ret)
 	{
 		j = 0;
 		while (split[i] && split[i][j])
-			if (!ft_isdigit(split[i][j++]))
-				return (ft_dfree((void **)split), NULL);
+			if (!(ft_isdigit(split[i][j++]) || !(!j && split[i][0] == '-')))
+				return (dlfree(&ret, split), NULL);
 		cont = malloc(sizeof(int));
-		*(int *)cont = ft_atoi(split[i]);
-		if (!i++)
-			ret = ft_lstnew(cont);
-		else
-			ft_lstadd_back(&ret, ft_lstnew(cont));
+		*(int *)cont = ft_atoi(split[i++]);
+		ft_lstadd_back(&ret, ft_lstnew(cont));
 	}
 	if (*split)
-		return (ft_dfree((void **)split), ret);
-	return (ft_dfree((void **)split), NULL);
+		return (dlfree(NULL, split), ret);
+	return (dlfree(NULL, split), NULL);
 }
 
 int	read_args(t_args args, t_list **stka)
 {
+	int		i;
 	t_list	*tmp;
 
+	i = 1;
+	tmp = NULL;
 	if (args.argc == 1)
 		return (printf("%s\n", args.argv[0]), 0);
 	else if (args.argc > 1)
 	{
-		// if (args.argc > 2)
-		if (args.argc == 2/*  && ft_strlen(args.argv[1]) */)
-			tmp = get_arg(args.argv[1], *stka);
-		// printf("-->%i\n", *(int *)tmp->content);
+		while (args.argv[i])
+			tmp = get_arg(args.argv[i++], tmp);
 		*stka = tmp;
-		if (*stka)
+		if (*stka && ft_lstsize(*stka) > 1)
 			return (1);
 		else
 			return (printf("Error\n"), 0);
@@ -73,12 +77,13 @@ int	main(int argc, char **argv)
 
 	args.argc = argc;
 	args.argv = argv;
+	stka = NULL;
 	i = read_args(args, &stka);
 	printf("ret :%i\n", i);
+	printf("len :%i\n", ft_lstsize(stka));
 	if (stka)
 	{
 		tmp = stka;
-		// printf("-->%i\n", *(int *)tmp->content);
 		while (i && stka)
 		{
 			printf("%i\n", *((int *)stka->content));
