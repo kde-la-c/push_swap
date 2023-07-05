af29bbcf51b3638db7014e84b85c25935a080e4b
kde-la-c <kdelacolinaplant@gmail.com>
2023-07-06 01:52:37 +0200

    scripting

--- optimization_scripts/prng.sh
+++ optimization_scripts/prng.sh
@@ -8,7 +8,6 @@ for (( i=6; i<800; i+=(i/3) ))
 do
 	for (( j=0; j<10; j++ ))
 	do
-		# echo -n "$i," >> "$file";
 		echo $(shuf -i 1-800 -n $i) >> "$file";
 	done
 	echo "" >> "$file";
--- optimization_scripts/run_program.sh
+++ optimization_scripts/run_program.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-input=output
-ret=$()
+input=nb_db.txt
+output=/out/output
+i=6
 
 while read -r line
 do
-	
+	len=$(echo "$line" | awk '{print NF}')
+	if (( $len > 1 )); then
+		for (( j=0; (( j<=12 && j<$i )); j++ ))
+		do
+			./push_swap $j $line | wc -l > $(output)$i.csv;
+			echo ";" > $(output)$i.csv;
+		done
+		echo -e "\n" > $(output)$i.csv;
+	else
+		(( i += i/3 ))
+	fi
 done < "$input"
