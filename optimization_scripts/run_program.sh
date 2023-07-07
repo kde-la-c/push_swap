1106c5f6fab9d77f24bf9e2fd1263458513b97fe
kde-la-c <kdelacolinaplant@gmail.com>
2023-07-07 02:02:22 +0200

    scripting

--- optimization_scripts/run_program.sh
+++ optimization_scripts/run_program.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 input=nb_db.txt
-output=/out/output
-i=6
+output=output
+i=5
 
 while read -r line
 do
 	len=$(echo "$line" | awk '{print NF}')
-	if (( $len > 1 )); then
-		for (( j=0; (( j<=12 && j<$i )); j++ ))
+	if (( $len > 3 )); then
+		for (( j=0; j<=12; j++ ))
 		do
-			./push_swap $j $line | wc -l > $(output)$i.csv;
-			echo ";" > $(output)$i.csv;
+			result= ./push_swap $j $line | wc -l >> "out/$i.csv";
+			printf $result "," >> "out/$i.csv";
+			# echo -n "," >> "out/$i.csv";
 		done
-		echo -e "\n" > $(output)$i.csv;
+		# echo -en "  \n" >> "out/$i.csv";
 	else
-		(( i += i/3 ))
+		(( i += i/3 ));
+		echo > "out/$i.csv";
 	fi
 done < "$input"
