363d9334ce44ed401933b8c45eef1c778f006645
kde-la-c <kdelacolinaplant@gmail.com>
2023-07-09 11:34:24 +0200

    all functional again

--- sort_100.c
+++ sort_100.c
@@ -18,7 +18,8 @@ int	**make_matrix(t_info info)
 	t_count	c;
 	int		**chunks;
 
-	c.i = 5;
+	// c.i = nbchunks;
+	c.i = 3;
 	c.j = 0;
 	c.l = 1;
 	if (!(chunks = (int **)malloc(sizeof(int *) * (c.i + 1))))
@@ -39,31 +40,6 @@ int	**make_matrix(t_info info)
 	return (chunks);
 }
 
-/* int	**make_matrix(t_info info)
-{
-	t_count	c;
-	int		**chunks;
-
-	c.i = 0;
-	c.k = 1;
-	chunks = (int **)malloc(sizeof(int *) * 6);
-	if (!chunks)
-		return (NULL);
-	while (c.i < 5)
-	{
-		c.j = (info.nbargs / 5) + (c.i < info.nbargs % 5) + 2;
-		chunks[c.i] = (int *)malloc(sizeof(int) * c.j);
-		chunks[c.i][0] = c.j - 2;
-		chunks[c.i][c.j - 1] = 0;
-		c.j = 1;
-		while (chunks[c.i][c.j])
-			chunks[c.i][c.j++] = c.k++;
-		c.i++;
-	}
-	chunks[5] = NULL;
-	return (chunks);
-} */
-
 /**
  * returns list index of closest chunk int to reach
 */
@@ -81,26 +57,18 @@ int	get_closest(t_list *stk, int *chunk, t_info *info)
 	while (stk)
 	{
 		c.j = 1;
-		//TODO fix this infinite loop
 		while (chunk[c.j])
 			if (!(*(int *)stk->content == chunk[c.j]))
 				c.j++;
 			else if (*(int *)stk->content == chunk[c.j++])
-			{
 				pos[c.k++] = c.i;
-			}
 		pos[c.k] = -1;
 		stk = stk->next;
 		c.i++;
 	}
 	ret = pos[0];
 	if (pos[0] != -1 && pos[0] > (*info).nbargs - pos[c.k - 1])
-		ret = pos[c.k - 1];
-	// for (int i = 0; pos[i - 1] > -1; i++)
-	// {
-	// 	printf("pos[%i]:%i, c.k:%li\n", i, pos[i], c.k);
-	// }
-	
+		ret = pos[c.k - 1];	
 	return (free(pos), ret);
 }
 
@@ -109,8 +77,8 @@ void	push_ordered(t_list **stka, t_list **stkb)
 	t_count	c;
 	t_info	a_info;
 
+	c.i = 0;
 	operation(&(*stka), &(*stkb), "pb");
-	// while ((*stkb)->next)
 	while (*stkb)
 	{
 		a_info = fill_info(*stka);
@@ -127,10 +95,7 @@ void	push_ordered(t_list **stka, t_list **stkb)
 				c.i += operation(&(*stka), &(*stkb), "rra");
 			operation(&(*stka), &(*stkb), "pb");
 			while (c.i > 0)
-			{
 				c.i -= operation(&(*stka), &(*stkb), "ra");
-				sleep(1);
-			}
 		}
 	}
 }
@@ -154,7 +119,6 @@ void	push_chunks(t_list **stka, int **chunks, t_info *info)
 	while (chunks[c.i])
 	{
 		c.j = get_closest(*stka, chunks[c.i], &(*info));
-		// printf("closest:%li\n", c.j);
 		if (c.j != -1)
 		{
 			tmp = ft_lstgetnode(*stka, c.j);
@@ -162,17 +126,11 @@ void	push_chunks(t_list **stka, int **chunks, t_info *info)
 				if (c.j <= (*info).nbargs / 2)
 					operation(&(*stka), NULL, "rra");
 				else
-				{
 					operation(&(*stka), NULL, "ra");
-					sleep(1);
-				}
 			operation(&(*stka), &stkb, "pa");
 		}
 		else
-		{
-			// printf("++\n");
 			c.i++;
-		}
 	}
 	push_ordered(&(*stka), &stkb);
 	ft_lstclear(&stkb, free);
