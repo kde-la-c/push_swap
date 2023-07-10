40fb5d90fa01e5f57a90c19c6b3ffaedea02488e
kde-la-c <kdelacolinaplant@gmail.com>
2023-07-10 17:42:04 +0200

    updated scripts

--- optimization_scripts/run_program.sh
+++ optimization_scripts/run_program.sh
@@ -8,15 +8,24 @@ while read -r line
 do
 	len=$(echo "$line" | awk '{print NF}')
 	if (( $len > 3 )); then
-		for (( j=0; j<=12; j++ ))
+		result=""
+		for (( j=0; j<=100; j++ ))
 		do
-			result= ./push_swap $j $line | wc -l >> "out/$i.csv";
-			printf $result "," >> "out/$i.csv";
+			count=$(./push_swap $j $line | wc -l)
+			result+=",$count"
 			# echo -n "," >> "out/$i.csv";
 		done
+		printf "%s\n" "${result:1}" >> "out/$i.csv"
 		# echo -en "  \n" >> "out/$i.csv";
 	else
 		(( i += i/3 ));
 		echo > "out/$i.csv";
+		result=""
+		for (( j=0; j<=100; j++ ))
+		do
+			result+=",$j"
+			# echo -n "," >> "out/$i.csv";
+		done
+		printf "%s\n" "${result:1}" >> "out/$i.csv"
 	fi
 done < "$input"
